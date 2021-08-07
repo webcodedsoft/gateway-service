451ad5a546d73ae71695ca992bd6da1cd9aa44cd
Adedeji Sikiru A <adedejisikiruadesola@gmail.com>
2021-08-07 05:09:43 +0100

    Complete laravel setup

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    
+    public function login(Request $request)
+    {
+        $email = 'demo@example.com';
+        $password = 'demo';
+        $username = 'demo_user';
+
+        if ($request->get('email') == $email && $request->get('password') == $password) {
+            $request->session()->put('user', $username);
+            if ($request->session()->has('user')) {
+                return response([ "status" => "success", "message" => "Login successful", "data" => session('user') ], 200);
+            }
+        } else {
+            return response([ "status" => "success", "message" => "Email or Password Incorrect" ], 400);
+        }
+    }
+
+    public function verifyToken(Request $request)
+    {
+                                    return response([ "status" => "success", "message" => "Unable to verified user", "data" => session('user')], 400);
+
+        if ($request->session()->has('user')) {
+                                    return response([ "status" => "success", "message" => "Unable to verified user" ], 400);
+
+            // if ($request->username == session('user')) {
+            //     return response([ "status" => "success", "message" => "User successfully verified", "data" => session('user') ], 200);
+            // } else {
+            //     return response([ "status" => "success", "message" => "Unable to verified user" ], 400);
+            // }
+            
+        }
+    }
+
+}
